f4869afc444525ce44b6b77ad81ec48c5bbc6247
training097 Student <training097@puhti-login1.bullx>
2022-06-28 00:21:26 +0300

    hello-world

--- mpi/hello-world/hello.cpp
+++ mpi/hello-world/hello.cpp
@@ -1,30 +1,29 @@
 #include <cstdio>
 #include <mpi.h>
 #include <iostream>
-
+#include <string>
 
 int main(int argc, char *argv[])
 {
     // TODO: say hello! in parallel
      // initialize
-     MPI_Init(NULL, NULL);
+     MPI_Init(&argc, &argv);
 
-     // get numbers 
-     // i). no processes and 
-     // ii). rank
-     int rank;
+    // obtain important information
+     int rank, number_processes, namelen;
+     char procname[MPI_MAX_PROCESSOR_NAME];
+
      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-     int number_processes;
      MPI_Comm_size(MPI_COMM_WORLD, &number_processes);
-    
+     MPI_Get_processor_name(procname, &namelen);
+
      // rank
      if (rank == 0){
-        std::cout << "Hello world from process with rank " << rank << std::endl;
         std::cout << "Total processes " << number_processes << std::endl;
      }
      else {
-         std::cout << "Hello world from process with rank " << rank << std::endl;
+         std::cout << "Hello world from processor " << procname <<
+                                      " with rank " << rank << std::endl;
      } 
 
      // finalize
