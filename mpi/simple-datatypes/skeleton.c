9e292f0c6fb1570dcbf2c9e25858f9d75af66dd4
training097 Student <training097@mahti-login11.mahti.csc.fi>
2022-06-28 10:50:17 +0300

    doing simple-datatypes

--- mpi/simple-datatypes/skeleton.c
+++ mpi/simple-datatypes/skeleton.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <vector>
 
 int main(int argc, char **argv)
 {
     int rank;
     int array[8][8];
     //TODO: Declare a variable storing the MPI datatype
+    MPI_Datatype newtype;
 
     int i, j;
 
@@ -37,11 +39,42 @@ int main(int argc, char **argv)
         }
     }
 
-    //TODO: Create datatype 
+    //TODO: Create datatype
+    int count, blocklen, stride;
+    //int displs[];
+
+    /*  A). sending col1
+    count = 8 ; // number of blocks
+    blocklen = 1; // number of elements in each block
+    stride = 8;  //displacement between the blocks
+    MPI_Type_vector(count, blocklen, stride, MPI_INT, &newtype);
+    */
+    
+
+
+    MPI_Type_indexed();
+    
+    
+    MPI_Type_commit(&newtype);
 
     //TODO: Send data
+    if (rank == 0){
+        // send the data
+        /* a). send the col1 of the matrix "array"
+        MPI_Send(&array[0][1], 1, newtype, 1, 1, MPI_COMM_WORLD);*/
+
+        /* b). send the rows with displacement*/
+        /* c). send a subarray */
+
+    }
+    else if (rank == 1){
+        // receive the data
+        // a). MPI_Recv(&array[0][1], 1, newtype, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
 
     //TODO: Free datatype
+    MPI_Type_free(&newtype);    
 
     // Print out the result on rank 1
     if (rank == 1) {
